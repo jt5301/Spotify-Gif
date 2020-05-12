134fb59f93c65fa5962560896663c65ddfb06465
John Tieu <johnson.tieu@gmail.com>
2020-05-11 20:12:58 -0400

    test

--- server/index.js
+++ server/index.js
@@ -73,6 +73,7 @@ const createApp = () => {
   //any remaining requests with an extension(.js, .css, etc.) send 404
   app.use((req, res, next) => {
     if (path.extname(req.path).length) {
+
       const err = new Error('Not found')
       err.status = 404
       next(err)
